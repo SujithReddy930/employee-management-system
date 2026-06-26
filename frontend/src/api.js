5b9c5d390af737c0a7409d1a4bb79d771bcbebb2
SujithReddy930 <sujithreddysanikommu336@gmail.com>
2026-06-26 16:16:36 +0530

    Update API baseURL to production backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://employee-management-system-production-79a4.up.railway.app/api'
 })
 
 // Department APIs
